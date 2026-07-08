d585dbc1b9b7f208acb4fc636c9b6d425f98e1ec
Mikakokeshi <mikakokeshi00124@gmail.com>
2026-07-08 12:15:50 +0900

    added: schema.tfにTODOアプリ用のスキーマ追加

--- terraform/schema.tf
+++ terraform/schema.tf
@@ -15,13 +15,33 @@ resource "snowflake_schema" "training_normalized" {
   comment = "Processed data for Streamlit."
 }
 
+
+# ==========================================
+# スキーマ定義（TODOアプリ用）
+# ==========================================
+resource "snowflake_schema" "training_raw_todo" {
+  database = snowflake_database.training_db.name
+  name     = "RAW_TODO" # TODO:ハンドアウトを参照し、適切なスキーマ名を設定してください
+  # S3から取り込んだメールの生データを保持するスキーマ。
+  comment = "Raw mail data ingested from S3."
+}
+
+resource "snowflake_schema" "training_normalized_todo" {
+  database = snowflake_database.training_db.name
+  name     = "NORMALIZED_TODO" # TODO:ハンドアウトを参照し、適切なスキーマ名を設定してください
+  # 加工・正規化済みデータを保持するスキーマ。
+  comment = "Processed data for Streamlit."
+}
 # ==========================================
 # スキーマへの権限付与
 # ==========================================
 resource "snowflake_grant_privileges_to_account_role" "training_schema_grants" {
   for_each = toset([
     "${snowflake_database.training_db.name}.${snowflake_schema.training_raw.name}",
-    "${snowflake_database.training_db.name}.${snowflake_schema.training_normalized.name}"
+    "${snowflake_database.training_db.name}.${snowflake_schema.training_normalized.name}",
+    # TODOアプリ用スキーマ追加
+    "${snowflake_database.training_db.name}.${snowflake_schema.training_raw_todo.name}",
+    "${snowflake_database.training_db.name}.${snowflake_schema.training_normalized_todo.name}"
   ])
   account_role_name = var.snowflake_role_name
   privileges = [
